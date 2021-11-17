eb3fd2a186ecb750ee3f6e5d8d20b7436cf71add
Murilovc <mvc.dev7cf@gmail.com>
2021-11-17 15:23:10 -0500

    Adicao do menu.php

---
+++ back-end/menu.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container px-5">
+                    <a class="navbar-brand" href="index.php">Gerenciamento de Resumos</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBarSupported"></button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item"><a class="nav-link" href="index.php">Início</a></li>
+                            <li class="nav-item"><a class="nav-link" href="index.php">Segunda tela</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+
+</nav>
