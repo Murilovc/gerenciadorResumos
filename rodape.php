4823cc56364bf84e99c68f3e68fe93a77130c9b4
Paulo Henrique Mendes Lira <pauloph47@gmail.com>
2021-12-23 22:11:48 -0500

    rodapé

---
+++ rodape.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+    <head>
+    <section class="">
+  <!-- Footer -->
+  
+  <footer class="text-center text-white" style="background-color: #00BFFF;">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: CTA -->
+      <section class="">
+        <p class="d-flex justify-content-center align-items-center">
+          <span class="me-3">Sistema de Avaliação de Resumos da PROEX</span>
+        </p>
+      </section>
+      <!-- Section: CTA -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      ©Copyright: Murilo Vieira Cardoso e Paulo Henrique Mendes Lira.
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+  
+</section>
+        
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
